a93514e86021a71d87093fb6b8ea1553be1cf440
Adam <adam@hariken.co>
2020-02-17 16:21:18 -0300

    dynamic crawl padding

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,19 @@ class App extends React.Component<any, any> {
     super(props);
     this.state = {
       content: 'Clique em um dos filmes para carregar o Crawl. Use o scroll para mover o texto.',
+      padding: 1
     };
   }
 
   fetchFilm(id: number) {
-    this.setState({content: 'Carregando...'});
+    this.setState({content: 'Carregando...', padding: 1});
     fetch(`https://swapi.co/api/films/${id}`, {
       method: 'POST',
       headers: {'Content-Type':'application/json'},
     })
       .then(response => response.json())
       .then(film => {
-        this.setState({content: film.opening_crawl});
+        this.setState({content: film.opening_crawl, padding: 200});
       })
       .catch(console.log);
   }
@@ -32,7 +33,7 @@ class App extends React.Component<any, any> {
   render() {
     return (
       <div className="container">
-        <Crawl content={this.state.content}></Crawl>
+        <Crawl content={this.state.content} padding={this.state.padding}></Crawl>
         <FilmList onSelected={this.onLoadFilmClick.bind(this)} />
       </div>
     )
--- src/Crawl.tsx
+++ src/Crawl.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './Crawl.css';
 
-class Crawl extends React.Component<{ content: string }> {
+class Crawl extends React.Component<{ content: string, padding?: number}> {
   render() {
-    const { content } = this.props;
+    const { content, padding } = this.props;
     const formated = textToHtml(content);
+    const style = {
+      margin: `${padding ? padding : 200}px 0 ${padding === 200 ? 1000 : 1111}px`
+    };
     return (
       <div className="crawl-container">
         <section>
           <span className="star"></span>
-          <article><p>{formated}</p></article>
+          <article><p style={style}>{formated}</p></article>
         </section>
       </div>
     );
